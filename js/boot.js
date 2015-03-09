8b1385f10e6fc601b340bc616d4a6c2c183319e8
mushopea <mushopea@hotmail.com>
2015-03-09 18:38:56 +0800

    boot state

---
+++ js/boot.js
@@ -0,0 +1,19 @@
+var Slider = {};
+Slider.Boot = function(game) {};
+
+Slider.Boot.prototype.preload = function() {
+    this.load.image('preloaderBar', 'assets/loading-bar.png');
+};
+
+Slider.Boot.prototype.create = function() {
+    console.log("Enter boot state.");
+
+    // set scale options
+    this.input.maxPointers = 1;
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.setScreenSize(true);
+    // start the Preloader state
+    this.state.start('Preloader');
+};
